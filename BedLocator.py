d9f7f0504897937dd5e04866edbbeeb4beacfba8
FinnDarle5 <144258994+FinnDarle5@users.noreply.github.com>
2023-12-20 14:00:34 +0530

    Update BedLocator.py

--- BedLocator.py
+++ BedLocator.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://suryanshroy.github.io/BedLocator/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
