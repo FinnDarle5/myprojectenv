cae57260935de1ee02712d868c850a2ec3b16a10
FinnDarle5 <144258994+FinnDarle5@users.noreply.github.com>
2023-12-20 15:20:27 +0530

    Update BedLocator.py

--- BedLocator.py
+++ BedLocator.py
@@ -5,11 +5,10 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # Update this line to include the origin of your frontend
-origins = ["https://suryanshroy.github.io"],
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
